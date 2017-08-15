f25cf8b1a8d665b68624364577e90f7345327665
filippofilip95 <fpapranecx@gmail.com>
2017-08-15 10:57:29 +0200

    <fix> Removed Asian languages.

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,10 +1,6 @@
 import I18n from 'react-native-i18n';
 import sk from './sk';
 import en from './en';
-import vi from './vi';
-import id from './id';
-import th from './th';
-import zh from './zh';
 import de from './de';
 
 I18n.fallbacks = true;
@@ -13,10 +9,6 @@ I18n.defaultLocale = 'en-EN';
 I18n.translations = {
   sk,
   en,
-  vi,
-  id,
-  th,
-  zh,
   de,
 };
 
